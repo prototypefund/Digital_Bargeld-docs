33c392da07b8c3b4d077d50d4b52f2c1e16eb55e
Marcello Stanisci <marcello.stanisci@inria.fr>
2016-03-08 10:44:39 +0100

    updating licensing information

--- exts/tsref.py
+++ exts/tsref.py
@@ -9,6 +9,8 @@
   A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
   You should have received a copy of the GNU Lesser General Public License along with
   TALER; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
+
+  @author Florian Dold
 """
 
 """
