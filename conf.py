14d13f43ef2f6c979509fc0ab41b2270730aada6
Florian Dold <florian.dold@gmail.com>
2019-08-29 17:51:12 +0200

    more PDFs

--- conf.py
+++ conf.py
@@ -233,6 +233,7 @@ latex_elements = {
 latex_documents = [
   ('taler-exchange-manual', 'taler-exchange-manual.tex', 'GNU Taler Exchange Manual', 'GNU Taler team', 'manual'),
   ('taler-merchant-manual', 'taler-merchant-manual.tex', 'GNU Taler Merchant Manual', 'GNU Taler team', 'manual'),
+  ('taler-merchant-api-tutorial', 'taler-merchant-api-tutorial.tex', 'GNU Taler Merchant API Tutorial', 'GNU Taler team', 'manual'),
   ('taler-bank-manual', 'taler-bank-manual.tex', 'GNU Taler Bank Manual', 'GNU Taler team', 'manual'),
   ('taler-backoffice-manual', 'taler-backoffice-manual.tex', 'GNU Taler Back Office Manual', 'GNU Taler team', 'manual'),
 ]
