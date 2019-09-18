8083c62562ffa5d5a212d91c6987f06495684313
Florian Dold <florian.dold@gmail.com>
2019-09-18 19:18:41 +0200

    ignore prebuilt

--- conf.py
+++ conf.py
@@ -98,7 +98,7 @@ release = '0.6.0pre1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '_exts', 'cf']
+exclude_patterns = ['_build', '_exts', 'cf', 'prebuilt']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
